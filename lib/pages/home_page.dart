5defd97ae259d848a9e3ee68f693ad185b90753b
thebjko <thebjko@gmail.com>
2022-05-30 10:24:17 +0900

    temporary

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,9 +10,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home'),
+    /// 참조1: https://www.flutterclutter.dev/flutter/tutorials/prevent-back-button-from-closing-the-app/2021/14074/
+    /// 참조2: https://protocoderspoint.com/flutter-double-back-press-to-exit-app/
+    /// 뒤로가기 두번 눌렀을 때 앱을 종료하는 방법에 관하여
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: const Scaffold(
+        body: Center(
+          child: Text('Home'),
+        ),
       ),
     );
   }
