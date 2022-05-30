05d6064bc8b47f87b295cbc212e8154d5beea0e8
thebjko <thebjko@gmail.com>
2022-05-30 11:31:55 +0900

    93. HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-import '../utils/logger.dart';
+import 'package:provider/provider.dart';
+import 'package:sangwook_firebase_authentication_app/pages/profile_page.dart';
+import 'package:sangwook_firebase_authentication_app/providers/auth/auth_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,14 +13,61 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    /// 참조1: https://www.flutterclutter.dev/flutter/tutorials/prevent-back-button-from-closing-the-app/2021/14074/
-    /// 참조2: https://protocoderspoint.com/flutter-double-back-press-to-exit-app/
-    /// 뒤로가기 두번 눌렀을 때 앱을 종료하는 방법에 관하여
     return WillPopScope(
       onWillPop: () async => false,
-      child: const Scaffold(
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Home'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                context.read<AuthProvider>().signout();
+              },
+              icon: Icon(Icons.exit_to_app),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ProfilePage();
+                    },
+                  ),
+                );
+              },
+              icon: Icon(Icons.exit_to_app),
+            ),
+          ],
+        ),
         body: Center(
-          child: Text('Home'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/flutter_favorite.png',
+                    width: 100,
+                  ),
+                  SizedBox(width: 20),
+                  Text(
+                    'Provider',
+                    style: TextStyle(fontSize: 42),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Provider is an awesome\nstate management library\nfor flutter!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
