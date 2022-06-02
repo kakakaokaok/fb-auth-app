a5a4faec478419528ea3e13a559861bed97b1bdc
thebjko <thebjko@gmail.com>
2022-06-02 09:47:36 +0900

    misc

--- lib/providers/profile/profile_provider.dart
+++ lib/providers/profile/profile_provider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:state_notifier/state_notifier.dart';
 
 import '../../models/custom_error.dart';
@@ -6,6 +5,7 @@ import '../../models/user_model.dart';
 import '../../repositories/profile_repository.dart';
 import 'profile_state.dart';
 
+/// ProfileRepository를 필요로 하기 때문에 LocatorMixin과 mixin.
 class ProfileProvider extends StateNotifier<ProfileState> with LocatorMixin {
   ProfileProvider() : super(ProfileState.initial());
 
